db3587ab76ecd659392831a64d95bcc4dd6dc5f7
Iarlyson Santana <Iarlyson.Santana@gmail.com>
2021-05-13 16:33:06 -0300

    model turma pronta

---
+++ back-end/src/models/class.ts
@@ -0,0 +1,19 @@
+import  { model, Schema} from ("mongoose");
+
+const Class = model(
+  "Class",
+  new Schema({
+    codigo: {
+      type: Number,
+    },
+    serie: {
+      type: String,
+      required: true,
+    },
+    descricao: {
+      type: String,
+      required: true,
+    },
+  })
+);
+export default Class;
